0d5ee447fab795d696c885973e1d3511092002a9
Jhonatan Parada <jhonatanparada499@gmail.com>
2026-02-10 12:39:17 -0500

    feat: import utility libraries

---
+++ recon_hand_written_digits.py
@@ -0,0 +1,11 @@
+# Authors: The scikit-learn developers
+# SPDX-License-Identifier: BSD-3-Clause
+
+# Standard scientific Python imports
+import matplotlib.pyplot as plt
+
+# Import datasets, classifiers and performance metrics
+from sklearn import datasets, metrics, svm
+from sklearn.model_selection import train_test_split
+
+print("a")
