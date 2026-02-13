5802e9e2b36b03463c75c80699b64fa2e6a2f012
Jhonatan Parada <jhonatanparada499@gmail.com>
2026-02-13 10:30:57 -0500

    fix: remap path to kaggle dataset

--- recon_written_digits/recognizing_written_digits.py
+++ recon_written_digits/recognizing_written_digits.py
@@ -18,7 +18,7 @@ import numpy as np
 import os
 
 
-PATH = "datasets/archive/"
+PATH = "../datasets/kaggle_written_digits/"
 
 # production
 categories = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
